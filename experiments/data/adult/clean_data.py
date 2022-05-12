5f5a6a3fa01fa031a75784b3d10ec25810f264ff
ganevgv <ganevgv@gmail.com>
2022-05-12 01:37:49 +0100

    update adult structure

--- experiments/data/adult/clean_data.py
+++ experiments/data/adult/clean_data.py
@@ -8,30 +8,32 @@ test_path = "adult.test"
 
 dtypes = {
     "age": "int",
+    "workclass": "category",
+    "fnlwgt": "int",
     "education": "category",
+    "education_num": "category",
     "marital_status": "category",
+    "occupation": "category",
     "relationship": "category",
+    "race": "category",
     "sex": "category",
+    "capital_gain": "int",
+    "capital_loss": "int",
+    "hours_per_week": "int",
+    "native_country": "category",
     "income": "category",
 }
 columns = list(dtypes.keys())
-all_columns = [
+
+tiny_columns = [
     "age",
-    "workclass",
-    "fnlwgt",
     "education",
-    "education_num",
     "marital_status",
-    "occupation",
     "relationship",
-    "race",
     "sex",
-    "capital_gain",
-    "capital_loss",
-    "hours_per_week",
-    "native_country",
     "income",
 ]
+tiny_dtypes = {k: v for k, v in dtypes.items() if k in tiny_columns}
 
 
 def get_bounds(df):
@@ -50,14 +52,12 @@ def get_bounds(df):
 # load data
 df = pd.read_csv(path,
                  sep=r",\s+",
-                 names=all_columns,
-                 usecols=columns,
+                 names=columns,
                  dtype=dtypes,
                  engine="python")
 test_df = pd.read_csv(test_path,
                       sep=r",\s+",
-                      names=all_columns,
-                      usecols=columns,
+                      names=columns,
                       dtype=dtypes,
                       engine="python",
                       skiprows=1)
@@ -65,14 +65,24 @@ test_df = pd.read_csv(test_path,
 
 # clean test labels
 test_df["income"] = test_df["income"].replace("<=50K.", "<=50K").replace(">50K.", ">50K")
+# clean dtypes
+test_df["native_country"] = test_df["native_country"].cat.set_categories(df["native_country"].cat.categories)
 
 
-# get df bounds
-df_bounds = get_bounds(df)
+# save adult
+df.to_pickle("adult.pkl.gz", compression="gzip")
+test_df.to_pickle("adult_test.pkl.gz", compression="gzip")
 
 
-# save
-df.to_pickle("tiny_adult.pkl.gz", compression="gzip")
-test_df.to_pickle("tiny_adult_test.pkl.gz", compression="gzip")
+# get tiny adult
+tiny_df = df[tiny_columns]
+tiny_test_df = test_df[tiny_columns]
+
+# get tiny adult bounds
+tiny_df_bounds = get_bounds(tiny_df)
+
+# save tiny adult
+tiny_df.to_pickle("tiny_adult.pkl.gz", compression="gzip")
+tiny_test_df.to_pickle("tiny_adult_test.pkl.gz", compression="gzip")
 with open("tiny_adult_bounds.pkl", "wb") as f:
-    pickle.dump(df_bounds, f, pickle.HIGHEST_PROTOCOL)
+    pickle.dump(tiny_df_bounds, f, pickle.HIGHEST_PROTOCOL)
